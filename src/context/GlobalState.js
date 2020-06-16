b63ac86f24665ad17ed06952a96e6417aea70a24
adil-innovation-lab <khanadilaltaf@gmail.com>
2020-06-16 23:24:28 +0500

    Updated GlobalState.js
    Removed dispatch from Reducer

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -19,7 +19,7 @@ export const GlobalContext = createContext(initialState);
 // Create a Provider for the Global Context
 export const GlobalProvider = ({ children }) => {
 
-    //const [state, dispatch] = useReducer(AppReducer, initialState)
+    const [state] = useReducer(AppReducer, initialState)
 
     return (
         <GlobalContext.Provider value={
