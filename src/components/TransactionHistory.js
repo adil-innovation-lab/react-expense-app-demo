3e7d837a96868a5386da24e5041985912ad6dcb2
adil-innovation-lab <khanadilaltaf@gmail.com>
2020-06-16 23:21:27 +0500

    Getting Transaction History from State
    Updated the Transaction history and created a Transaction component to get transactions from the state using the context
    
    Disabled the Reducer

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react';
+
+// Import the Global Context
+import { GlobalContext } from '../context/GlobalState';
+
+// Import Transactions
+import { Transaction } from './Transaction'
 
 export const TransactionHistory = () => {
+
+    const { transactions } = useContext(GlobalContext);
+
     return (
         <div>
             <h3>Transaction History</h3>
             <ul className="list">
-                <li className="plus">
-                    Project 1 Income
-                    <span>$1,000</span>
-                    <button className="delete-btn">X</button>
-                </li>
-                <li className="minus">
-                    Project 1 Salaries
-                    <span>-$500</span>
-                    <button className="delete-btn">X</button>
-                </li>
+                {transactions.map(transaction => (
+                    <Transaction key={transaction.id} transaction={transaction} />
+                    )
+                )}
             </ul>
         </div>
     )
